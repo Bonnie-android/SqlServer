a6f3356795e3987ee298527ff16023c937352068
Bonnie S <bonnie_sterling@hotmail.com>
2017-12-24 08:42:36 -0500

    Add files via upload

---
+++ Optional Parameters in Sql Server Stored Procedures.sql
@@ -0,0 +1,23 @@
+---- Optional Parameters in SQl Server Stored Procedures
+---- Parameters of a SQL Server stored procedure can be made optional by specifying default values
+---- In this case if we don't specify any parameters we receive all records
+Alter Proc spSearchEmployees
+@Name varchar(50) = NULL,
+@Title varchar(20) = NULL,
+@Salary money = NULL
+as
+BEGIN
+   SELECT [ID]
+      ,[Name]
+      ,[Title]
+      ,[Salary]
+  FROM [ProductDB].[dbo].[Employees]
+  WHERE (Name = @Name OR @Name IS NULL) AND
+  (Title = @Title OR @Title IS NULL)  
+END
+
+spSearchEmployees ---- returns the full list
+spSearchEmployees @Name ='Mary-Lee Scott' --- returns a single record
+spSearchEmployees @Title= 'Manager' --- returns list of all managers
+
+
